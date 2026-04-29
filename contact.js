5c540498f9305d44e15be153fdb3b47c0ad31744
Raju k <115688979+raju7k@users.noreply.github.com>
2026-04-30 00:06:16 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -16,7 +16,7 @@ document.getElementById("contactForm")
 
     try{
 
-        const response = await fetch("http://localhost:8080/send-email",{
+        const response = await fetch("https://contactform-pe9g.onrender.com",{
 
             method: "POST",
 
